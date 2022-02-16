a61fbfb7744aa827cd4998ef55540a78cb53a187
bl2-18 <bl2-18@DESKTOP-NQAFT0U>
2022-02-16 09:48:23 +0300

    Proje dosyası ekle.

---
+++ NTP_2022/Giris.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTP_2022
+{
+    internal class Giris
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MERHABA DÜNYA");
+        }
+    }
+}
